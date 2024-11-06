5c32261c87dcb239896ea7186f69ed1495292f6c
macastrorale <macastrorale@gmail.com>
2024-11-06 11:36:38 -0500

    Update git.php

--- api/secciones/git.php
+++ api/secciones/git.php
@@ -4,8 +4,11 @@ SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
 *****/
 
 $git = [
-"titulo" => "XXX",
-"autor" => "XXX",
-"resumen" => "XXX",
+"titulo" => "Github",
+"autor" => "Javier Pastor",
+"resumen" => "HOY SE HABLA DE
+China Bill Gates Elon Musk El problema de los tres cuerpos DANA Tesla Invento Aceite de oliva Mejores móviles Android Auto
+Si eres desarrollador, Google lo tiene claro: más que picar código, pronto acabarás revisando el que pica la IA 
+Si eres desarrollador, Google lo tiene claro: más que picar código, pronto acabarás revisando el que pica la IA",
 ];
 ?>
