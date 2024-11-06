8c947dba9f359f0aa95ca7ae04c6a19a69e62a03
macastrorale <macastrorale@gmail.com>
2024-11-06 11:33:16 -0500

    Update empleo.php

--- api/secciones/empleo.php
+++ api/secciones/empleo.php
@@ -4,8 +4,8 @@ SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
 *****/
 
 $empleo = [
-"titulo" => "XXX",
-"autor" => "XXX",
-"resumen" => "XXX",
+"titulo" => "Empleabilidad",
+"autor" => "El Peruano",
+"resumen" => "El Índice Global de Competitividad y Talento 2023, elaborado por el Instituto Europeo de Administración de Negocios (INSEAD) y el Descartes Institute for the Future, que mide la capacidad de los países para atraer y retener talento, revela que América Latina está rezagada frente a Europa. En esta región, Chile es el país mejor posicionado, ocupando el puesto 34 entre 134 países. Perú, en cambio, se encuentra en el puesto 82, y en términos de empleabilidad, alcanzó una puntuación de 44.38, lo que lo coloca en el lugar 110 del ranking.",
 ];
 ?>
