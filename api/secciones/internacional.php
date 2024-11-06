22ac551ce44be92c97d123deccd7f37eda691d4c
macastrorale <macastrorale@gmail.com>
2024-11-06 11:29:45 -0500

    Update internacional.php

--- api/secciones/internacional.php
+++ api/secciones/internacional.php
@@ -4,8 +4,8 @@ SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
 *****/
 
 $internacional = [
-"titulo" => "XXX",
-"autor" => "XXX",
-"resumen" => "XXX",
+"titulo" => "DONALD TRUMP PRESIDENTE DE LOS EE.UU",
+"autor" => "CARLOS MORENOS",
+"resumen" => "Donald John Trump es un empresario, personalidad televisiva y político conservador​​ estadounidense. Miembro del Partido Republicano, es el presidente electo de los Estados Unidos, tras ganar las elecciones presidenciales de 2024.",
 ];
 ?>
