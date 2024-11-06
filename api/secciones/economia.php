456cac9825b2dbd7983633eaa577ee22a6bc5f3e
macastrorale <macastrorale@gmail.com>
2024-11-06 11:28:41 -0500

    Update economia.php

--- api/secciones/economia.php
+++ api/secciones/economia.php
@@ -4,8 +4,9 @@ SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
 *****/
 
 $economia = [
-"titulo" => "XXX",
-"autor" => "XXX",
-"resumen" => "XXX",
-];
+"titulo" => "Descubren una nueva forma de generar energía limpia a partir de residuos plásticos",
+"autor" => "Carlos López",
+"resumen" => "Investigadores de la Universidad de Stanford han desarrollado un proceso innovador que convierte los residuos plásticos en energía limpia. Este avance podría ayudar a mitigar tanto el problema del plástico en el medio ambiente como la dependencia de fuentes de energía no renovables. El proceso, que utiliza un catalizador especial, ofrece una alternativa más eficiente y menos costosa a las tecnologías existentes.",
+
+  ];
 ?>
