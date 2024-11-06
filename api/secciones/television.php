bcc6d1c7952af6b51444795735336ab517546196
macastrorale <macastrorale@gmail.com>
2024-11-06 11:42:06 -0500

    Update television.php

--- api/secciones/television.php
+++ api/secciones/television.php
@@ -4,8 +4,8 @@ SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
 *****/
 
 $television = [
-"titulo" => "XXX",
-"autor" => "XXX",
-"resumen" => "XXX",
+"titulo" => "Empezamos Más Conectados",
+"autor" => "TV Peru",
+"resumen" => "Hoy hablamos sobre el acoso y la violencia infantil, y junto a UNICEF Perú exploramos los derechos de los niños y niñas a la salud, en el marco de nuestra campaña,Para cada infancia, derechos"
 ];
 ?>
