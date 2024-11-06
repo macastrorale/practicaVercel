87fe872f93f3c920d9996b42fcdcbb46e14c0b12
macastrorale <macastrorale@gmail.com>
2024-11-06 11:31:28 -0500

    Update nacional.php

--- api/secciones/nacional.php
+++ api/secciones/nacional.php
@@ -4,8 +4,8 @@ SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
 *****/
 
 $nacional = [
-"titulo" => "XXX",
-"autor" => "XXX",
-"resumen" => "XXX",
+"titulo" => "Situación del hambre en el Perú se encuentra igual de crítica que en el 2022, según el último Índice Global del Hambre",
+"autor" => "RPP Noticias",
+"resumen" => "El Índice Global del Hambre que fue publicado hoy y que recoge cifras del 2023, evidencia que no ha habido mejora en temas alimentarios dentro del país. Diez regiones se encuentran en situación de hambre "grave", indica el reporte analizado por RPP Data.",
 ];
 ?>
