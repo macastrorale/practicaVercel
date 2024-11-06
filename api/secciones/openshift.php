d641688d385971a68d74c0d65c283180fdaef2e5
macastrorale <macastrorale@gmail.com>
2024-11-06 11:37:25 -0500

    Update openshift.php

--- api/secciones/openshift.php
+++ api/secciones/openshift.php
@@ -4,8 +4,8 @@ SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
 *****/
 
 $openshift = [
-"titulo" => "XXX",
-"autor" => "XXX",
-"resumen" => "XXX",
+"titulo" => "Red Hat OpenShift Container Platform 4",
+"autor" => "SEVEN ICE",
+"resumen" => "Se han encontrado dos vulnerabilidades críticas en Red Hat OpenShift. Las vulnerabilidades permiten la ejecución de código arbitrario y el secuestro de nodos que ejecutan OpenShift. Existen medidas de mitigación de las vulnerabilidades que deberían implementarse sin demora.",
 ];
 ?>
