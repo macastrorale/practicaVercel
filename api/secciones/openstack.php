4c1634dd3e5c39f895f9c4fc90449372f17828a8
macastrorale <macastrorale@gmail.com>
2024-11-06 11:38:19 -0500

    Update openstack.php

--- api/secciones/openstack.php
+++ api/secciones/openstack.php
@@ -4,8 +4,8 @@ SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
 *****/
 
 $openstack = [
-"titulo" => "XXX",
-"autor" => "XXX",
-"resumen" => "XXX",
+"titulo" => " OpenStack la tendencia actual del mercado para la nube privada a escala empresarial",
+"autor" => "SARDINA",
+"resumen" => "Sabemos que elegir la solución adecuada de computación en nube privada no es tarea fácil, pero elegir el software de computación en nube adecuado garantizará el éxito de tu nube privada. La plataforma madura y altamente flexible de OpenStack se ha convertido en un motor de innovación para empresas de una amplia gama de sectores, permitiendo a los usuarios operar tanto sistemas heredados como aplicaciones nativas de la nube a través de un único marco. ",
 ];
 ?>
