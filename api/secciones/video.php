a0643cc1e4efde3852bb89c20fbe6b06529f744e
macastrorale <macastrorale@gmail.com>
2024-11-06 11:42:34 -0500

    Update video.php

--- api/secciones/video.php
+++ api/secciones/video.php
@@ -4,8 +4,8 @@ SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
 *****/
 
 $video = [
-"titulo" => "XXX",
-"autor" => "XXX",
-"resumen" => "XXX",
+"titulo" => "El sorprendente video de un robot que aprende a bailar como un humano",
+"autor" => "Ana Martínez",
+"resumen" => "Un video viral ha capturado la atención de millones de personas en internet, mostrando a un robot diseñado por investigadores de la Universidad de Tokio, que ha aprendido a bailar como un ser humano. Usando inteligencia artificial y algoritmos avanzados, el robot no solo replica los movimientos, sino que también sincroniza sus movimientos con la música. Este avance podría tener aplicaciones en la robótica de entretenimiento, así como en la mejora de la interacción entre humanos y máquinas.",
 ];
 ?>
