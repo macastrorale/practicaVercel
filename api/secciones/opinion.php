614a054792eac76b62c288df2d8fe97832950c23
macastrorale <macastrorale@gmail.com>
2024-11-06 11:32:23 -0500

    Update opinion.php

--- api/secciones/opinion.php
+++ api/secciones/opinion.php
@@ -4,8 +4,8 @@ SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
 *****/
 
 $opinion = [
-"titulo" => "XXX",
-"autor" => "XXX",
-"resumen" => "XXX",
+"titulo" => "Empleabilidad y educación",
+"autor" => "El Peruano",
+"resumen" => "La revolución digital ha transformado profundamente tanto el ámbito laboral como el educativo, no solo en el Perú, sino en todo el mundo. Hoy en día, los estudiantes ya no solo necesitan habilidades técnicas propias de su carrera. También requieren de las llamadas power skills, como el pensamiento crítico, la inteligencia emocional y la comunicación efectiva, que les permitirán enfrentar la complejidad de un entorno digital en constante evolución.",
 ];
 ?>
