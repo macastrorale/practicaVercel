8c138b816f4c65dc4e1b00607b2f94a5469f648e
macastrorale <macastrorale@gmail.com>
2024-11-06 11:34:46 -0500

    Update formacion.php

--- api/secciones/formacion.php
+++ api/secciones/formacion.php
@@ -4,8 +4,8 @@ SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
 *****/
 
 $formacion = [
-"titulo" => "XXX",
-"autor" => "sistemas@uns",
-"resumen" => "XXX",
+"titulo" => "Ciudad tristeza",
+"autor" => "Cristóbal Manuel Sánchez",
+"resumen" => "En su día (los años noventa), estas fotos fueron concebidas como imágenes para contar una historia concreta, sucesos, noticias o vida cotidiana de la ciudad, para la sección de Madrid del diario EL PAÍS. Pero con el tiempo su autor descubrió que lo que estaba haciendo en realidad era un relato con el que expresar sus propias emociones. Ahora han sido convertidas en un diario de la experiencia del autor con la sociedad de su entorno",
 ];
 ?>
