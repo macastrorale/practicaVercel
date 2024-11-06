b3008d3c4abd548904b8caac5899940b816e6cfd
macastrorale <macastrorale@gmail.com>
2024-11-06 11:35:46 -0500

    Update gente.php

--- api/secciones/gente.php
+++ api/secciones/gente.php
@@ -4,8 +4,8 @@ SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
 *****/
 
 $gente = [
-"titulo" => "XXX",
-"autor" => "XXX",
-"resumen" => "XXX",
+"titulo" => "Recortar personal no debe ser la primera medida de eficiencia en el 2025",
+"autor" => "Ana María Martínez",
+"resumen" => "Cuando el problema de ineficiencia es generalizado en la operación, es probable que la causa principal no sea un exceso de personas, sino un exceso de tareas innecesarias",
 ];
 ?>
