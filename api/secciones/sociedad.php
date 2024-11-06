3c411f61fe9447749d3b01d4b69e6a212a82bb01
macastrorale <macastrorale@gmail.com>
2024-11-06 11:39:52 -0500

    Update sociedad.php

--- api/secciones/sociedad.php
+++ api/secciones/sociedad.php
@@ -4,8 +4,8 @@ SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
 *****/
 
 $sociedad = [
-"titulo" => "XXX",
-"autor" => "XXX",
-"resumen" => "XXX",
+"titulo" => "Las lágrimas de un niño al ver su colegio destrozado por la DANA",
+"autor" => "La Opinion De Murcia",
+"resumen" => "En la localidad de Valencia, un niño de 8 años no pudo contener las lágrimas al ver cómo su colegio había quedado devastado tras el paso de la DANA (Depresión Aislada en Niveles Altos), que afectó gravemente a varias zonas de la Comunidad Valenciana. Las fuertes lluvias y vientos causaron daños estructurales, inundaciones y la caída de árboles. La comunidad educativa y las autoridades locales se han movilizado para evaluar los daños y comenzar las labores de reconstrucción. El incidente resalta la vulnerabilidad de muchas infraestructuras ante fenómenos meteorológicos extremos, cada vez más frecuentes debido al cambio climático.",
 ];
 ?>
